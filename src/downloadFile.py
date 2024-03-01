86890438eecf826d9d77a09b44f0bc89be873ee6
Programmer <Programmer@ua>
2024-03-01 15:49:25 +0200

    added messagebox

--- src/downloadFile.py
+++ src/downloadFile.py
@@ -10,11 +10,10 @@ def download_video(link):
     try:
         yt = YouTube(link, on_complete_callback=windowMessage.open_window_message("Message...", "Download complete!"))
         # yt = YouTube(link, use_oauth=True, allow_oauth_cache=True)
-        messagebox.showinfo("Сообщение", "Перейдите по сслылке")
+        # messagebox.showinfo("Сообщение", "Перейдите по сслылке")
 
 
         is_download = yt.streams.get_highest_resolution().download("videos")
-        
         return is_download # Download Complete!
     except Exception as e:
         windowMessage.open_window_error(e)
